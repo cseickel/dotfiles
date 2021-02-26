1571dd65b53ab70fbf05aa4c2d714b3429d864ab
Chris Seickel <cseickel@rwjf.org>
2021-02-26 10:33:03 -0500

    fix start docker scripts

--- start-arch-linux-docker.sh
+++ start-arch-linux-docker.sh
@@ -2,4 +2,6 @@
 docker run -it \
     -v arch-linux-volume:/home/arch \
     -v /var/run/docker.sock:/var/run/docker.sock \
+    -p 127.0.0.1:8080:8080 -p 127.0.0.1:8180:8180 \
+    -p 127.0.0.1:5002:5002 -p 127.0.0.1::7777:7777 \
     cseickel/arch-linux tmux
