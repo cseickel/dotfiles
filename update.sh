bc9a8874cc7e09332f6db05d03fb0c428b5c0afd
Chris Seickel <cseickel@gmail.com>
2021-04-29 07:25:47 -0400

    fix update script

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git --git-dir ~/.dotfiles.git pull
+git --git-dir ~/.dotfiles/.git pull
 ~/.dotfiles/install
 nvim --headless -u ~/.config/nvim/plugin-install.vim -c \
-    "PlugUpgrade | echo '...' | PlugClean! | echo '...' | PlugInstall | echo '...' | PlugUpdate | qa" \
+    "PlugUpgrade | echo '...' | PlugClean! | echo '...' | PlugInstall | echo '...' | PlugUpdate | TSUpdate | CocUpdate | qa" \
