0940bdcf86495da2d9375a3cc9cac3afe6c251b4
Chris Seickel <cseickel@gmail.com>
2022-07-07 17:27:12 +0000

    ...

--- config/nvim/lua/plugins/config/nvim-treesitter.lua
+++ config/nvim/lua/plugins/config/nvim-treesitter.lua
@@ -34,26 +34,6 @@ return function()
     }
   }
 
-  local ts_parsers = require("nvim-treesitter.parsers")
-  vim.api.nvim_create_autocmd("BufEnter", {
-    group = vim.api.nvim_create_augroup("treesitter", {clear = true}),
-    pattern = { "*" },
-    callback = function()
-      local ft = vim.bo.filetype
-      if not ft then
-        return
-      end
-      local parser = ts_parsers.filetype_to_parsername[ft]
-      if not parser then
-        return
-      end
-      local is_installed = ts_parsers.has_parser(ts_parsers.ft_to_lang(ft))
-      if not is_installed then
-        vim.cmd("TSInstall " .. parser)
-      end
-    end,
-  })
-
   require('nvim-treesitter.configs').setup {
     textobjects = {
       select = {
@@ -75,11 +55,11 @@ return function()
         set_jumps = true, -- whether to set jumps in the jumplist
         goto_next_start = {
           ["]m"] = "@function.outer",
-          ["]]"] = "@block.outer",
+          ["]["] = "@block.outer",
         },
         goto_next_end = {
           ["]M"] = "@function.outer",
-          ["]["] = "@block.outer",
+          ["]]"] = "@block.outer",
         },
         goto_previous_start = {
           ["[m"] = "@function.outer",
