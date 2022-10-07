a4f128a8177820f3fb9ebeb925e09418d8aec364
Chris Seickel <cseickel@gmail.com>
2022-10-07 01:03:08 +0000

    bootstrap packer, aut install fonts

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 local function c(name)
   local succuss, func = pcall(require, "plugins.config." .. name)
   if succuss and func then
