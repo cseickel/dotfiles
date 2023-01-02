049e443c26037b050e22f0478f3bb3a0bac33ee7
Chris Seickel <cseickel@gmail.com>
2023-01-02 09:16:51 -0600

    fix nvim-cmp

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -13,48 +13,39 @@ return {
     "hrsh7th/cmp-nvim-lsp-document-symbol",
     "hrsh7th/cmp-nvim-lsp-signature-help",
     "rcarriga/cmp-dap",
-    {
-      "onsails/lspkind-nvim",
-      opt = true,
-      event = "InsertEnter",
-      config = function()
-        require("lspkind").init({
-          mode = "symbol_text",
-          preset = "default",
-          symbol_map = {
-            Text = "",
-            Method = "",
-            Function = "",
-            Constructor = "",
-            Variable = "",
-            Class = "",
-            Interface = "",
-            Module = "",
-            Property = "",
-            Unit = "",
-            Value = "",
-            Enum = "了",
-            Keyword = "",
-            Snippet = "﬌",
-            Color = "",
-            File = "",
-            Folder = "",
-            EnumMember = "",
-            Constant = "",
-            Struct = "",
-            Operator = "",
-          },
-        })
-      end,
-    },
-    {
-      "David-Kunz/cmp-npm",
-      config = function()
-        require("cmp-npm").setup()
-      end,
-    },
+    "onsails/lspkind-nvim",
+    "David-Kunz/cmp-npm",
   },
   config = function()
+    require("cmp-npm").setup()
+    require("lspkind").init({
+      mode = "symbol_text",
+      preset = "default",
+      symbol_map = {
+        Text = "",
+        Method = "",
+        Function = "",
+        Constructor = "",
+        Variable = "",
+        Class = "",
+        Interface = "",
+        Module = "",
+        Property = "",
+        Unit = "",
+        Value = "",
+        Enum = "了",
+        Keyword = "",
+        Snippet = "﬌",
+        Color = "",
+        File = "",
+        Folder = "",
+        EnumMember = "",
+        Constant = "",
+        Struct = "",
+        Operator = "",
+      },
+    })
+
     local cmp = require("cmp")
     cmp.setup({
       enabled = function()
