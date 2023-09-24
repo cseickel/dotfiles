7d23270d0e1bf5f442d86bd5048699700fd570bb
Chris Seickel <cseickel@gmail.com>
2023-09-24 09:07:42 -0500

    fix merge conflict

--- config/nvim/lua/plugins/nvim-lsp-file-operations.lua
+++ config/nvim/lua/plugins/nvim-lsp-file-operations.lua
@@ -1,18 +1,7 @@
 return {
   'antosha417/nvim-lsp-file-operations',
-<<<<<<< Updated upstream
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-neo-tree/neo-tree.nvim",
   }
 }
-=======
-  dev = false,
-  config = { debug = false },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-neo-tree/neo-tree.nvim",
-  },
-}
-
->>>>>>> Stashed changes
