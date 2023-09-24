c495ba6f6f3174083d972934625c39a604aa6e44
Chris Seickel <cseickel@gmail.com>
2023-09-24 09:00:54 -0500

    add nvim-lsp-file-operations

--- config/nvim/lua/plugins/nvim-lsp-file-operations.lua
+++ config/nvim/lua/plugins/nvim-lsp-file-operations.lua
@@ -1,7 +1,18 @@
 return {
   'antosha417/nvim-lsp-file-operations',
+<<<<<<< Updated upstream
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-neo-tree/neo-tree.nvim",
   }
 }
+=======
+  dev = false,
+  config = { debug = false },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
+  },
+}
+
+>>>>>>> Stashed changes
